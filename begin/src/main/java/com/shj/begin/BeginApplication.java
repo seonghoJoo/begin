372245064c9c831973a0cae3d64046132413ef73
seonghJoo <etkl8888@naver.com>
2024-05-07 15:45:36 +0900

    init: start project

---
+++ begin/src/main/java/com/shj/begin/BeginApplication.java
@@ -0,0 +1,13 @@
+package com.shj.begin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeginApplication.class, args);
+    }
+
+}
---
+++ begin/src/test/java/com/shj/begin/BeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shj.begin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
