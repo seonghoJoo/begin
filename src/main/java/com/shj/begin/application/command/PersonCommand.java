2cdb0e3dc4ba5636f057a1cf210f663d0da45b28
seonghJoo <etkl8888@naver.com>
2024-07-03 21:59:54 +0900

    update: Person API 예제 (command)

--- src/main/java/com/shj/begin/application/command/PersonCommand.java
+++ src/main/java/com/shj/begin/application/command/PersonCommand.java
@@ -6,14 +6,21 @@ import com.shj.begin.domain.Person;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Slf4j
 @RequiredArgsConstructor
 public abstract class PersonCommand implements PersonInPort {
 
     protected PersonOutPort personOutPort;
+    Map<String, String> instance = new ConcurrentHashMap<>();
 
     @Override
     public Person savePerson(Person person) {
+
+        Map<String, String> local = new HashMap<>(); // 로컬 변수는 동시성 이슈 발생 불가능 => HashMap 사용
         personOutPort.savePerson(person);
         return null;
     }
